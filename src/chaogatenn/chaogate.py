0e158d14481be0440b54271dbaf9b10687a63931
ParticularlyPythonicBC <anil@anilr.dev>
2024-10-20 06:56:47 -0400

    n input extension to chaogates

--- src/chaogatenn/chaogate.py
+++ src/chaogatenn/chaogate.py
@@ -30,6 +30,28 @@ class ChaoGate(eqx.Module):
             self.Map(self.X0 + x1 * self.DELTA + x2 * self.DELTA) - self.X_THRESHOLD
         )
 
+@jaxtyped(typechecker=typechecker)
+class NChaoGate(eqx.Module):
+    DELTA: Float[Array, ""]
+    X0: Float[Array, ""]
+    X_THRESHOLD: Float[Array, ""]
+    Map: MapLike
+
+    def __init__(
+        self, DELTA: ArrayLike, X0: ArrayLike, X_THRESHOLD: ArrayLike, Map: MapLike
+    ):
+        self.DELTA = jnp.array(DELTA)
+        self.X0 = jnp.array(X0)
+        self.X_THRESHOLD = jnp.array(X_THRESHOLD)
+        self.Map = Map
+
+    @typechecker
+    def __call__(self, x: Bool[Array, "n"]) -> Float[Array, ""]:
+
+        signal = (x * self.DELTA).sum()
+        return jax.nn.sigmoid(
+            self.Map(self.X0 + signal) - self.X_THRESHOLD
+        )
 
 if __name__ == "__main__":
     from chaogatenn.maps import LogisticMap
@@ -39,3 +61,7 @@ if __name__ == "__main__":
 
     x = jnp.array([1, 0], dtype=bool)
     print(chaogate(x))
+
+    chaogate_n_input = NChaoGate(DELTA=0.1, X0=0.5, X_THRESHOLD=0.5, Map=logistic_map)
+
+    print(chaogate_n_input(x))
